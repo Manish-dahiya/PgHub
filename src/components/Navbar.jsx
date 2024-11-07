02e85ce9e6b7a04e14143fe24b5249b0230206a6
Manish-dahiya <manishdahiya5720@gmail.com>
2024-11-07 22:17:34 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='h-20 w-full fixed top-0 border border-b-slate-700'>
+      
+    </div>
+  )
+}
+
+export default Navbar
